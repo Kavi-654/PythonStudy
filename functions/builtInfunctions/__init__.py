2b6b126f4b2bd355e14f2800b4af99c7548412d8
Kavi-654 <kavinayasri654@gmail.com>
2026-04-20 20:01:04 +0530

    Initial Commit

---
+++ functions/builtInfunctions/__init__.py
@@ -0,0 +1,11 @@
+
+from functools import reduce
+# # map fucntions
+# a=[1,2,3,4]
+# res=list(map(lambda x:x**2),a)
+# print(res)
+#
+# # reduce function
+#
+# a=[1,2,3,4]
+# ans=list(reduce(lambda x,y :if x>y else y,a))
